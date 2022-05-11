62f379f0dfa37dc2389a1378901ffb5fbb7887e0
dwjcarson <45634378+dwjcarson@users.noreply.github.com>
2022-05-11 17:42:10 +0100

    changed sprite to blue/green one, changed velocity x to 150

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 tiles.setTilemap(tilemap`level1`)
 scene.setBackgroundImage(assets.image`background`)
-let mySprite = sprites.create(assets.image`truck3`, SpriteKind.Player)
+let mySprite = sprites.create(assets.image`truck1`, SpriteKind.Player)
 mySprite.ay = 500
-mySprite.vx = 100
+mySprite.vx = 150
 scene.cameraFollowSprite(mySprite)
