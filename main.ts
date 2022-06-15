f6639703b18bb8e1cfd50290357ec91ba8e4fc99
dwjcarson <45634378+dwjcarson@users.noreply.github.com>
2022-06-15 15:38:37 +0100

    velocity X = 200

--- main.ts
+++ main.ts
@@ -2,5 +2,4 @@ tiles.setTilemap(tilemap`level1`)
 scene.setBackgroundImage(assets.image`background`)
 let mySprite = sprites.create(assets.image`truck1`, SpriteKind.Player)
 mySprite.ay = 500
-mySprite.vx = 150
-scene.cameraFollowSprite(mySprite)
+mySprite.vx = 200
